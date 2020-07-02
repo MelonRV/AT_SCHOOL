ade8e6321b49b4cf796f72e2b6ff7ccdad04d0a0
MelonRV <61627102+MelonRV@users.noreply.github.com>
2020-07-03 00:39:12 +0400

    подпилил вывод

--- src/main/java/TestCompareObject.java
+++ src/main/java/TestCompareObject.java
@@ -7,7 +7,7 @@ public class TestCompareObject {
         ArrayList<Integer>list1 = new ArrayList<Integer>();
 
         System.out.println("Тест метода сравнения объектов: " + CompareObjects.compare(list,list1));
-        System.out.println("Тест метода перевода объектов в строку и сравнивнения: " + CompareObjects.compareHashCode(list,list1));
-        System.out.println("Тест метода сравнивнения хэш-кодов объектов: " + CompareObjects.compareToString(list,list1));
+        System.out.println("Тест метода перевода объектов в строку и сравнивнения: " + CompareObjects.compareToString(list,list1));
+        System.out.println("Тест метода сравнения хэш-кодов объектов: "+ CompareObjects.compareHashCode(list,list1));
     }
 }
